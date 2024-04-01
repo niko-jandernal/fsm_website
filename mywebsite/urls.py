1bca3474c1926acfbddf843f5bf28937dadc546e
niko_university <n.jandernal2@newcastle.ac.uk>
2024-04-01 22:05:16 +0100

    fixed issues with the sidebar @ base.html, added any missing pages to create a working side nav

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -5,5 +5,13 @@ urlpatterns = [
     path('', views.home), # This is the default page (home page of the website)
     path('home', views.home, name='home'),
     path('explore', views.explore, name='explore'),
+    path('notifications', views.notifications, name='notifications'),
+    path('boards', views.boards, name='boards'),
+    path('polls', views.polls, name='polls'),
+    path('discussions', views.discussions, name='discussions'),
+    path('news', views.news, name='news'),
+    path('create', views.create, name='create'),
+    path('signin', views.signin, name='signin'),
+
 
 ]
--- mywebsite/views.py
+++ mywebsite/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render, HttpResponse
-from .models import TodoItem
+from django.shortcuts import render
+
+
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
 
+
 def explore(request):
     return render(request, "explore.html")
+
+
+def notifications(request):
+    return render(request, "notifications.html")
+
+
+def boards(request):
+    return render(request, "boards.html")
+
+
+def polls(request):
+    return render(request, "polls.html")
+
+
+def discussions(request):
+    return render(request, "discussions.html")
+
+
+def news(request):
+    return render(request, "news.html")
+
+
+def create(request):
+    return render(request, "create.html")
+
+
+def signin(request):
+    return render(request, "signin.html")
